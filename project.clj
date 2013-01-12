8426ef669101e26bb9490cc0a599cb882afbe87f
Paul Lam <paul@quantisan.com>
2013-01-12 19:46:30 +0000

    add MIT license

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject cascalog-more-taps "0.3.1-SNAPSHOT"
-  :description "Taps for Cascalog"
-  :url "https://github.com/nathanmarz/cascalog-contrib"
+  :description "More taps for Cascalog"
+  :url "https://github.com/Quantisan/cascalog.more-tap"
   :min-lein-version "2.0.0"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/MIT"}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]
                                   [cascalog "1.10.0"]
                                   [cascading/cascading-hadoop "2.0.4"]
